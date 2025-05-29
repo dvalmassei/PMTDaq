0c3b266bc0dfe7cfd3ad707ae895338029cd75a4
dvalmassei <dvalmassei@vt.edu>
2025-05-29 13:40:31 -0400

    update self_trigger_setup.py

--- HV_scan.py
+++ HV_scan.py
@@ -20,11 +20,11 @@ def configure_digitizer(digitizer:CAEN_DT5742_Digitizer):
    	digitizer.set_acquisition_mode('sw_controlled')
    	digitizer.set_ext_trigger_input_mode('disabled')
    	digitizer.write_register(0x811C, 0x000D0001) # Enable busy signal on GPO.
-   	digitizer.set_fast_trigger_mode(enabled=False)
-   	digitizer.set_fast_trigger_digitizing(enabled=False)
+   	digitizer.set_fast_trigger_mode(enabled=True)
+   	digitizer.set_fast_trigger_digitizing(enabled=True)
    	digitizer.enable_channels(group_1=True, group_2=False)
    	digitizer.set_fast_trigger_threshold(22222)
-   	#digitizer.set_fast_trigger_DC_offset(V=0)
+   	digitizer.set_fast_trigger_DC_offset(V=0)
    	digitizer.set_post_trigger_size(0)
    	for ch in [0]:
            digitizer.set_trigger_polarity(channel=ch, edge='falling')
