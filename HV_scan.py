194456685775633b561e785494ebdbac32499630
dvalmassei <dvalmassei@vt.edu>
2025-05-30 14:58:29 -0400

    update HV_scan.py

--- HV_scan.py
+++ HV_scan.py
@@ -20,7 +20,7 @@ def configure_digitizer(digitizer:CAEN_DT5742_Digitizer):
    	digitizer.set_acquisition_mode('sw_controlled')
    	digitizer.set_ext_trigger_input_mode('disabled')
    	digitizer.write_register(0x811C, 0x000D0001) # Enable busy signal on GPO.
-   	digitizer.set_fast_trigger_mode(enabled=True)
+   	#digitizer.set_fast_trigger_mode(enabled=True)
    	digitizer.set_fast_trigger_digitizing(enabled=True)
    	digitizer.enable_channels(group_1=True, group_2=False)
    	#digitizer.set_fast_trigger_threshold(22222)
@@ -74,7 +74,7 @@ def check_error_code(code):
         raise RuntimeError(f'libCAENDigitizer has returned error code {code}.')
     
     
-def main(dc_offset=-0.3, self_trigger_threshold=3700, n_events=1000, low_HV=1500, high_HV=2000, n_steps=10):
+def main(dc_offset=-0.3, self_trigger_threshold=10, n_events=1000, low_HV=1900, high_HV=2000, n_steps=10):
     
     ########## setup ##########
     HV = CAENDesktopHighVoltagePowerSupply(port='/dev/ttyACM0') # Open the connection.
@@ -122,7 +122,6 @@ def main(dc_offset=-0.3, self_trigger_threshold=3700, n_events=1000, low_HV=1500
     data = [] #Pandas DataFrame
     
     with digitizer:
-        digitizer.start_acquisition()
         print('Digitizer is enabled!')
         for voltage in [low_HV,high_HV,n_steps]:
             temp_data = [] #List
@@ -143,7 +142,6 @@ def main(dc_offset=-0.3, self_trigger_threshold=3700, n_events=1000, low_HV=1500
             print('Now appending to DataFrame...')
             data = pd.concat(data,convert_dicitonaries_to_data_frame(temp_data,voltage))
     
-    digitizer.stop_acquisition()
     print('Acquisition complete.')
     
     
